4debce25689ac47c838776de296a00b0bbfa2354
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-06-07 11:00:16 +0200

    Finished oour local framework

---
+++ LocoFramework/LocoFramework/LocoFramework.h
@@ -0,0 +1,18 @@
+//
+//  LocoFramework.h
+//  LocoFramework
+//
+//  Created by Manyuchi, Carrington C on 2024/06/07.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocoFramework.
+FOUNDATION_EXPORT double LocoFrameworkVersionNumber;
+
+//! Project version string for LocoFramework.
+FOUNDATION_EXPORT const unsigned char LocoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFramework/PublicHeader.h>
+
+
---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by Manyuchi, Carrington C on 2024/06/07.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Did some stuff"
+    }
+}
